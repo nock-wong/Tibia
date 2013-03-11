0391ef6849207fe7e1bb1bbebb798867db490152
Nick <nick@radiatorlabs.com>
2013-03-11 01:57:04 -0400

    Add JPEG decompression.
    Can now extract values from DICOM images.

--- code/dicom/load.py
+++ code/dicom/load.py
@@ -1,19 +1,41 @@
 import dicom
+import errno
 import os
+import subprocess
 
-dicomdir = "C:\Users\Nick\Desktop\Tibia\data\input\Juan_Cantelejo"
+dicomdir = os.path.join("C:","\Users","Nick","Desktop","Tibia","data","input","Juan_Cantelejo")
 
-ds = dicom.read_file("{dir}/DICOMDIR".format(dir=dicomdir))
+# dcmdjpeg options dcmfile-in dcmfile-out
+dcmdjpeg = os.path.join("C:","\dcmtk-bin","dcmjpeg","apps","Debug","dcmdjpeg.exe")
+
+def make_sure_path_exists(path):
+    try:
+        os.makedirs(path)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+
+DICOM = os.path.join(dicomdir, "DICOMDIR")
+ds = dicom.read_file(DICOM)
+
+dirOut = os.path.join(dicomdir, "decompressed")
 
 pixel_data = list()
-
 for record in ds.DirectoryRecordSequence:
     if record.DirectoryRecordType == "IMAGE":
         # Extract the relative path to the DICOM file
-        path = os.path.join(dicomdir, *record.ReferencedFileID)
-        dcm = dicom.read_file(path)
+        pathIn = os.path.join(dicomdir, *record.ReferencedFileID)
+#        print record.ReferencedFileID
+        make_sure_path_exists(os.path.join(dicomdir, "decompressed", *record.ReferencedFileID[0:-1]))
+        pathOut = os.path.join(dicomdir, "decompressed", *record.ReferencedFileID)
 
-        # Now get your image data
-        pixel_data.append(dcm.pixel_array)
+        if 1:
+	        # Decompress JPEG files
+	        subprocess.call([dcmdjpeg, pathIn, pathOut])
+	        
+	        dcm = dicom.read_file(pathOut)
+
+	        # Now get your image data
+	        pixel_data.append(dcm.pixel_array)
 
 print pixel_data
