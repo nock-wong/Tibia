40d9b877cf74353b3ce0dcd0326fd56bd51caf92
Nick <nick@radiatorlabs.com>
2013-04-04 02:01:00 -0400

    create data model, models.py

--- code/server/dicom_web/models.py
+++ code/server/dicom_web/models.py
@@ -3,21 +3,30 @@ from django.db import models
 class Dicom(models.Model):
     base_dir = models.CharField(max_length=200)
 
+    def __unicode__(self):
+        return self.base_dir
+
 class Patient(models.Model):
     dicom = models.ForeignKey(Dicom)
     patientId = models.CharField(max_length = 10)
     name = models.CharField(max_length = 10)
     birthdate = models.CharField(max_length = 10)
     sex = models.CharField(max_length = 10)
+
+    def __unicode__(self):
+        return self.name
     
 class Study(models.Model):
     dicom = models.ForeignKey(Dicom)
+    studyId = models.CharField(max_length = 10)
     date = models.CharField(max_length = 10)
     time = models.CharField(max_length = 10)
     accessionNumber = models.CharField(max_length = 10)
     description = models.CharField(max_length = 10)
     instanceUID = models.CharField(max_length = 10)
-    studyId = models.CharField(max_length = 10)
+
+    def __unicode__(self):
+        return self.studyId
     
 class Series(models.Model):
     study = models.ForeignKey(Study)
@@ -29,6 +38,9 @@ class Series(models.Model):
     description = models.CharField(max_length = 10)
     instanceUID = models.CharField(max_length = 10)
     number = models.CharField(max_length = 10)
+
+    def __unicode__(self):
+        return self.description
 
 class Image(models.Model):
     series = models.ForeignKey(Series)
@@ -45,3 +57,6 @@ class Image(models.Model):
     contentTime = models.CharField(max_length = 10)
     imagePosition = models.CharField(max_length = 10)
     imageOrientation = models.CharField(max_length = 10)
+
+    def __unicode__(self):
+        return self.instanceNumber
